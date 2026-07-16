6a74dd6efe5235362a0b4c9bda03150920580f6a
Aayush Kumar <ayushankitaara@gmail.com>
2026-07-16 22:49:38 +0530

    Initialize React Vite frontend

---
+++ frontend-code/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-gray-500'>
+      <h1 className='text-5xl text-green-400'>FrontEnd Code</h1>
+    </div>
+  )
+}
+
+export default App
